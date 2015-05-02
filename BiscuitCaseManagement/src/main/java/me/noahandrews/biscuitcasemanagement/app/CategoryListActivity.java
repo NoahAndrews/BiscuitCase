5cfe4411c04094c48c0800ade9fa7ff075485fac
NoahAndrews <NoahAndrews@users.noreply.github.com>
2015-05-02 19:19:25 -0400

    Added stuff to ItemListActivity

--- BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/CategoryListActivity.java
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/CategoryListActivity.java
@@ -5,7 +5,6 @@ import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
--- BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
@@ -5,10 +5,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import me.noahandrews.biscuitcaselibrary.Category;
 import me.noahandrews.biscuitcaselibrary.Item;
+import me.noahandrews.biscuitcaselibrary.ItemListAdapter;
 import me.noahandrews.biscuitcaselibrary.ItemsDataSource;
 
 import java.util.ArrayList;
@@ -21,6 +28,7 @@ public class ItemListActivity extends AppCompatActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private ListView mDrawerList;
 
+    private Category mCategory;
     private ArrayList<Item> mItems;
     private ItemsDataSource mDataSource;
 
@@ -29,6 +37,42 @@ public class ItemListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
 
+        mDataSource = ItemsDataSource.HOST_INSTANCE;
+        if(!mDataSource.isOpened()){
+            mDataSource.open();
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerList = (ListView)findViewById(R.id.nav_drawer_list);
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.NavSections)));
+
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView){
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mItems = mDataSource.getItems(mCategory);
+
+        RecyclerView list = (RecyclerView)findViewById(R.id.itemList);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        //list.setAdapter(new ItemListAdapter(this,))
     }
     
     
