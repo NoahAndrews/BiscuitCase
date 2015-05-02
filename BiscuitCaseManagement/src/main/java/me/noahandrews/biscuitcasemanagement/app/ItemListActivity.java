3fc3d3e289ae0eb0a1eea3d75ab5dadc1db08014
NoahAndrews <NoahAndrews@users.noreply.github.com>
2015-05-02 19:30:56 -0400

    Removed unneeded imports

--- BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
@@ -1,7 +1,6 @@
 package me.noahandrews.biscuitcasemanagement.app;
 
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
