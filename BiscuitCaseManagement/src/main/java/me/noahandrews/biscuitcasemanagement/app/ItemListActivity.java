179ed6f860bb659e891b818487e60eb494b2b578
NoahAndrews <NoahAndrews@users.noreply.github.com>
2015-05-02 18:46:44 -0400

    Added ItemListActivity

---
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
@@ -0,0 +1,55 @@
+package me.noahandrews.biscuitcasemanagement.app;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import me.noahandrews.biscuitcaselibrary.ItemsDataSource;
+
+import java.util.ArrayList;
+
+
+public class ItemListActivity extends AppCompatActivity {
+    private CharSequence mTitle;
+    private CharSequence mDrawerTitle;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
+
+    private ArrayList<Category> mCategories;
+    private ItemsDataSource mDataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+
+    }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_item_list, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_settings) {
+            return true;
+        }
+        
+        return super.onOptionsItemSelected(item);
+    }
+}
