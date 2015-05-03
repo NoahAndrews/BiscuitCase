4a63f67a9e16a44746464135122f6984b4968640
NoahAndrews <NoahAndrews@users.noreply.github.com>
2015-05-02 22:11:18 -0400

    More code towards completing ItemListActivity

--- BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/ItemListActivity.java
@@ -1,21 +1,28 @@
 package me.noahandrews.biscuitcasemanagement.app;
 
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import com.software.shell.fab.ActionButton;
 import me.noahandrews.biscuitcaselibrary.Category;
 import me.noahandrews.biscuitcaselibrary.Item;
-import me.noahandrews.biscuitcaselibrary.ItemListAdapter;
 import me.noahandrews.biscuitcaselibrary.ItemsDataSource;
+import me.noahandrews.biscuitcaselibrary.Section;
 
 import java.util.ArrayList;
 
@@ -28,7 +35,7 @@ public class ItemListActivity extends AppCompatActivity {
     private ListView mDrawerList;
 
     private Category mCategory;
-    private ArrayList<Item> mItems;
+    private static ArrayList<Item> mItems;
     private ItemsDataSource mDataSource;
 
     @Override
@@ -71,10 +78,39 @@ public class ItemListActivity extends AppCompatActivity {
 
         RecyclerView list = (RecyclerView)findViewById(R.id.itemList);
         list.setLayoutManager(new LinearLayoutManager(this));
-        //list.setAdapter(new ItemListAdapter(this,))
+        list.setAdapter(new ItemListAdapter(mItems));
+
+        Drawable plusIcon = getResources().getDrawable(R.drawable.fab_plus_icon); //must continue to use deprecated method for now. The replacement requires API 21+
+        Drawable plusIconTinted = DrawableCompat.wrap(plusIcon);
+        DrawableCompat.setTint(plusIconTinted, getResources().getColor(R.color.fab_material_black));
+        ActionButton addCategoryButton = (ActionButton)findViewById(R.id.actionButton);
+        addCategoryButton.setImageDrawable(plusIconTinted);
+        addCategoryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(ItemListActivity.this);
+                dialog.setTitle("Add new item");
+                dialog.setMessage("Enter a name.");
+                final EditText nameField = new EditText(ItemListActivity.this);
+                nameField.setInputType(InputType.TYPE_CLASS_TEXT);
+                dialog.setView(nameField);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Item newItem = new Item(nameField.getText().toString());
+                        mDataSource.addItem(newItem);
+                        ItemListActivity.addItem(newItem);
+                    }
+                });
+                dialog.setNegativeButton("Cancel", null);
+                dialog.show();
+            }
+        });
     }
     
-    
+    static void addItem(Item item){
+        mItems.add(item);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
