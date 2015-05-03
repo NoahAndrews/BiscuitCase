7dff30bb3325696a939f3af7e26c1e7b6e141976
NoahAndrews <NoahAndrews@users.noreply.github.com>
2015-05-02 22:14:30 -0400

    Created NewItemActivity

---
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/NewItemActivity.java
@@ -0,0 +1,39 @@
+package me.noahandrews.biscuitcasemanagement.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class NewItemActivity extends ActionBarActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+    }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_item, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_settings) {
+            return true;
+        }
+        
+        return super.onOptionsItemSelected(item);
+    }
+}
