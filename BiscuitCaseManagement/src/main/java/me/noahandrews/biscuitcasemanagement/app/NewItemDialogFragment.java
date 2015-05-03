903beaf6e4663d615486b79358d1179526bdf4e4
NoahAndrews <NoahAndrews@users.noreply.github.com>
2015-05-03 00:33:40 -0400

    Replaced dialog Activity with dialog Fragment

---
+++ BiscuitCaseManagement/src/main/java/me/noahandrews/biscuitcasemanagement/app/NewItemDialogFragment.java
@@ -0,0 +1,55 @@
+package me.noahandrews.biscuitcasemanagement.app;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import me.noahandrews.biscuitcaselibrary.Item;
+
+public class NewItemDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add new item");
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_new_item,null);
+        builder.setView(dialogView);
+        final EditText nameField = (EditText)dialogView.findViewById(R.id.nameField);
+        final EditText priceField = (EditText)dialogView.findViewById(R.id.priceField);
+        final CheckBox quantityLimitedCheckbox = (CheckBox)dialogView.findViewById(R.id.quantityLimitedCheckbox);
+        final TextView quantityAvailableLable = (TextView)dialogView.findViewById(R.id.quantityLabel);
+        final EditText quantityAvailableField = (EditText)dialogView.findViewById(R.id.quantityField);
+        quantityLimitedCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((CheckBox)v).isChecked()){
+                    quantityAvailableField.setVisibility(View.VISIBLE);
+                    quantityAvailableLable.setVisibility(View.VISIBLE);
+                } else {
+                    quantityAvailableField.setVisibility(View.GONE);
+                    quantityAvailableLable.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = nameField.getText().toString();
+                double price = Double.parseDouble(priceField.getText().toString());
+                boolean limitedQuantity = quantityLimitedCheckbox.isChecked();
+                int quantity = Integer.parseInt(quantityAvailableField.getText().toString());
+                Item newItem = new Item(name, price, null, limitedQuantity, quantity);
+                //send newItem to activity
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        return builder.create();
+    }
+}
